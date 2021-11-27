46e445242f1cf37c6228db3f34f8ee84c3e7e555
emrizki <emrizkidev@gmail.com>
2021-11-27 10:15:50 +0700

    create Square & Input component boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Square from './Square';
+import Input from './Input';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Square />
+      <Input />
     </div>
   );
 }
